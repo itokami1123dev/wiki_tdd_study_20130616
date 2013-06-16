52dfc197c62888cb9ff6edb5d80f2c1024375ab4
itokami1123 <kamiari819dev@gmail.com>
2013-06-16 15:18:33 +0900

    last commit

--- tests.js
+++ tests.js
@@ -6,6 +6,10 @@ test( "=ではじめ=で終わるとh1タグでくくられるとOK", function()
 	equal( wiki('=test='), '<h1>test</h1>');
 });
 
+test( "==ではじめ==で終わるとh2タグでくくられるとOK", function() {
+	equal( wiki('==test=='), '<h2>test</h2	>');
+});
+
 test( "= ではじめ =で終わるとh1タグでくくられるとOK", function() {
 	equal( wiki('= test ='), '<h1>test</h1>');
 });
@@ -17,4 +21,20 @@ test( "= *ではじめ *=で終わるとh1タグでくくられるとOK", functi
 test( "= =は<h1> </h1>でOK", function() {
 	equal( wiki('= ='), '<h1></h1>');
 });
+
+test( "¥= ¥=をエスケープとして表示でOK", function() {
+	equal( wiki('¥= ¥='), '= =');
+	equal( wiki('¥= TDD ¥='), '= TDD =');
+	equal( wiki('¥= TDD ='), '= TDD =');
+	equal( wiki('= TDD ¥='), '= TDD =');
+});
+
+test( "=aaaは=aaaでOK", function() {
+	equal( wiki('=aaa'), '=aaa');
+});
+
+test( "aaa=はaaa=でOK", function() {
+	equal( wiki('aaa='), 'aaa=');
+});
+
 
--- wiki.js
+++ wiki.js
@@ -1,7 +1,13 @@
 function wiki(str) {
+
 	var headerPattern1 = /^= *([^= ]*) *=$/;
+    var headerPattern1 = new RegExp("^= *([^= ]*) *=$/");
+
 	if ( headerPattern1 .test(str) ){
 		return str.replace(headerPattern1, "<h1>$1</h1>");
 	}
+
+	str = str.replace(/¥=/g, "=");
+
 	return str;
 }
