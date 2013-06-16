e83547cf58e51a1f0851322fa9398f3aba7ef012
itokami1123 <kamiari819dev@gmail.com>
2013-06-16 14:08:46 +0900

    三角測量のテストを減らしました

--- tests.js
+++ tests.js
@@ -1,34 +1,20 @@
 test( "入力値が出力されるとOK", function() {
 	equal( wiki('a'), 'a');
-	equal( wiki('b'), 'b');
 });
 
 test( "=ではじめ=で終わるとh1タグでくくられるとOK", function() {
 	equal( wiki('=test='), '<h1>test</h1>');
-	equal( wiki('=test2='), '<h1>test2</h1>');
 });
 
 test( "= ではじめ =で終わるとh1タグでくくられるとOK", function() {
 	equal( wiki('= test ='), '<h1>test</h1>');
-	equal( wiki('= test2 ='), '<h1>test2</h1>');
 });
 
 test( "= *ではじめ *=で終わるとh1タグでくくられるとOK", function() {
 	equal( wiki('=  test  ='), '<h1>test</h1>');
-	equal( wiki('=   test2   ='), '<h1>test2</h1>');
 });
 
 test( "= =は<h1> </h1>でOK", function() {
 	equal( wiki('= ='), '<h1></h1>');
-	//equal( wiki('=   test2   ='), '<h1>test2</h1>');
 });
-
-/*
-test( "3桁数字でOK", function() {
-  equal( false, isZipCode('123') );
-  equal( true, isZipCode('456') );
-  equal( true, isZipCode(456) );
-});
-
-*/
 
